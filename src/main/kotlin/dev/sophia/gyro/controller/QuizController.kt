7af5b8f463a2846a801a73a5f757f0ce6796db4e
Sophia_quq <sofia0627sofia@gmail.com>
2026-03-05 18:28:02 +0800

    xml

--- src/main/kotlin/dev/sophia/gyro/controller/QuizController.kt
+++ src/main/kotlin/dev/sophia/gyro/controller/QuizController.kt
@@ -1,6 +1,7 @@
 package dev.sophia.gyro.controller
 
 import dev.sophia.gyro.QuizService
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -17,29 +18,33 @@ class alldata(private val quizService: QuizService) {
     )
     private val records = mutableListOf<AnswerRecord>()
 
+    val xmlString = object {}.javaClass.getResource("/Questions.xml")!!.readText()
+
+    @Tag(name = "data")
+    @GetMapping("/xml/data")
+    fun getXMLData(): String {
+        if(xmlString != "") return xmlString
+        return "null"
+    }
+
+    @Tag(name = "data")
     @GetMapping("/data")
     fun getAllInfo(): quizData {
         return quizService.getData()
     }
 
+    @Tag(name = "sounds")
     @GetMapping("/sounds")
     fun getAllSounds(): List<soundsAssets> = list
 
-
+    @Tag(name = "records")
     @GetMapping("/records")
     fun getAllRecords(): List<AnswerRecord> = records
 
+    @Tag(name = "post")
     @PostMapping("/records/post")
     fun addRecord(@RequestBody record: AnswerRecord) {
         records.add(record)
-    }
-
-    val xmlString = object {}.javaClass.getResource("/Questions.xml")!!.readText()
-
-    @GetMapping("/xml/data")
-    fun getXMLData(): String {
-        if(xmlString != "") return xmlString
-        return "null"
     }
 }
 
